ebc69190224cbf03ad28921f66f4398bb7435479
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-09-08 18:04:35 +0100

    for of object loop

--- script2.js
+++ script2.js
@@ -1,59 +1,46 @@
-const items = [
+let players = [
     {
-        name: 'Bike',
-        price: 750,
+        name: "Roger Federer",
+        rank: 1
     },
     {
-        name: 'TV',
-        price: 200,
+        name: "Rafel Nadal",
+        rank: 2
     },
     {
-        name: 'Album',
-        price: 12,
+        name: "Nalbandaian",
+        rank: 12
     },
     {
-        name: 'Book',
-        price: 20,
+        name: "Andy Murray",
+        rank: 14
     },
     {
-        name: 'Pen',
-        price: 5,
+        name: "Pete Sampras",
+        rank: 3
     },
     {
-        name: 'Laptop',
-        price: 250,
+        name: "Andy Roddick",
+        rank: 4
     },
     {
-        name: 'Phone',
-        price: 150,
+        name: "Rod Laver",
+        rank: 190
     },
     {
-        name: 'Bedsheets',
-        price: 25
-    }
+        name: "Andre Agassi",
+        rank: 11
+    },
+    {
+        name: "Novak Djokovic",
+        rank: 5
+    },
+    {
+        name: "Arthur Ashe",
+        rank: 8
+    },
 ]
 
-const filteredItems = items.filter(item => {
-    return item.price >= 100
-})
-
-const itemName = items.map(item => {
-    return item.price
-})
-
-const foundItem = items.find(item => {
-    return item.name === 'Phone'
-})
-
-const hasInexpensiveItems = items.some(item => item.price < 50);
-const total = items.reduce((currentPrice, item) => {
-    return currentPrice+= item.price
-  }, 0) 
-
-const hasPrice = items.every(item => item.price)
-console.log(filteredItems);
-console.log(itemName);
-console.log(foundItem.price);
-console.log(hasInexpensiveItems);
-console.log("has price: " + hasPrice);
-console.log("Total: " + total);
+for(player of players) {
+    player.rank <= 10 ? console.log(player.name + " is on the top ten list"): null;
+}
