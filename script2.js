8f5bd5d999cc9a34a36e07d35495fa09dd9b1074
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-09-08 23:53:05 +0100

    solved duplicates

--- script2.js
+++ script2.js
@@ -1,46 +1,19 @@
-let players = [
-    {
-        name: "Roger Federer",
-        rank: 1
-    },
-    {
-        name: "Rafel Nadal",
-        rank: 2
-    },
-    {
-        name: "Nalbandaian",
-        rank: 12
-    },
-    {
-        name: "Andy Murray",
-        rank: 14
-    },
-    {
-        name: "Pete Sampras",
-        rank: 3
-    },
-    {
-        name: "Andy Roddick",
-        rank: 4
-    },
-    {
-        name: "Rod Laver",
-        rank: 190
-    },
-    {
-        name: "Andre Agassi",
-        rank: 11
-    },
-    {
-        name: "Novak Djokovic",
-        rank: 5
-    },
-    {
-        name: "Arthur Ashe",
-        rank: 8
-    },
-]
+function duplicateCount(text){
+    //...
+    obj = {};
+    counter = 0
+    for(let i = 0; i < text.length; i++) {
+      if(!obj[text[i]]) {
+        obj[text[i]] = 1;
+      } else {
+         if(obj[text[i]] < 2) {
+             obj[text[i]] += 1;
+             counter++
+         }
+      }
+    }
+    console.log(counter);
+    return obj;
+  }
 
-for(player of players) {
-    player.rank <= 10 ? console.log(player.name + " is on the top ten list"): null;
-}
+  console.log(duplicateCount('afa0000olabiii'));
